4ac2758d68382c67ecc6940a9515d75c1ed63f5b
Sheikh Antor <antoralways2025@gmail.comm>
2022-08-22 20:22:50 +0600

    add my name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='bg-purple-600 p-4 '>
         
-         <h1 className='text-center md:text-3xl  text-white '>Simple To do Aplication with Redux</h1>  
+         <h1 className='text-center md:text-3xl  text-white '>Simple To do Aplication with Redux <sup>Sheikh Antor</sup> </h1>  
 
     </div>
   )
